f768d3521ba07e3388d62eb89fb1b1182c1ab475
Arman <cybertechik@protonmail.com>
2026-05-10 11:24:24 +0330

    refactored and better password strenghth value object

--- domain/value_objects/password_strength.py
+++ domain/value_objects/password_strength.py
@@ -7,7 +7,6 @@ class PasswordStrength:
     def __post_init__(self):
         if not isinstance(self.score, int):
             raise TypeError("Password Strength.score must be an integer")
-
         if not (0 <= self.score <= 4):
             raise ValueError("Password Strength must be between 0 and 4")
 
@@ -16,3 +15,22 @@ class PasswordStrength:
 
     def is_strong(self) -> bool:
         return self.score >= 3
+
+    @staticmethod
+    def from_password(raw_password: str) -> "PasswordStrength":
+   
+        score = 0
+        length = len(raw_password)
+
+        if length >= 8:
+            score += 1
+        if any(c.isupper() for c in raw_password):
+            score += 1
+        if any(c.isdigit() for c in raw_password):
+            score += 1
+        if any(c in "!@#$%^&*()_+-=[]{}|;:',.<>?/" for c in raw_password):
+            score += 1
+
+        score = min(max(score, 0), 4)
+
+        return PasswordStrength(score=score)
