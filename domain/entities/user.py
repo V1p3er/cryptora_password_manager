34999937662492067dda63b165760eacf7caf9d6
Arman <cybertechik@protonmail.com>
2026-05-16 12:58:46 +0330

    refactored user entity and fixed some logic flaws

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,44 +1,61 @@
 from dataclasses import dataclass
-from domain.value_objects.password_strength import PasswordStrength
+
 from domain.value_objects.username import Username
 from domain.value_objects.userid import UserId
 from domain.value_objects.password_hash import PasswordHash
 from domain.value_objects.updated_at import UpdatedAt
 from domain.value_objects.created_at import CreatedAt
 
-
-@dataclass
+@dataclass(slots=True)
 class User:
-    user_id: UserId
-    username: Username
-    password_hash: PasswordHash
-    password_strength: PasswordStrength
-    created_at: CreatedAt
-    updated_at: UpdatedAt
+    _user_id: UserId
+    _username: Username
+    _password_hash: PasswordHash
+    _created_at: CreatedAt
+    _updated_at: UpdatedAt
 
     @classmethod
     def create(
         cls,
-        *,
         user_id: UserId,
         username: Username,
         password_hash: PasswordHash,
-        password_strength: PasswordStrength,
         created_at: CreatedAt,
     ) -> "User":
+
+        updated_at = UpdatedAt(created_at.value)
+
         return cls(
-            user_id=user_id,
-            username=username,
-            password_hash=password_hash,
-            password_strength=password_strength,
-            created_at=created_at,
-            updated_at=UpdatedAt.from_created(created_at),
+            _user_id=user_id,
+            _username=username,
+            _password_hash=password_hash,
+            _created_at=created_at,
+            _updated_at=updated_at
         )
 
-    def change_password(self, new_password_hash: PasswordHash, new_password_strength: PasswordStrength) -> None:
-        if new_password_hash == self.password_hash:
+    def change_password(self, new_password_hash: PasswordHash) -> None:
+        if new_password_hash == self._password_hash:
             raise ValueError("New password hash must be different")
 
-        self.password_hash = new_password_hash
-        self.password_strength = new_password_strength
-        self.updated_at = UpdatedAt.now()
+        self._password_hash = new_password_hash
+        self._updated_at = UpdatedAt.now()
+
+    @property
+    def user_id(self) -> UserId:
+        return self._user_id
+
+    @property
+    def username(self) -> Username:
+        return self._username
+
+    @property
+    def password_hash(self) -> PasswordHash:
+        return self._password_hash
+
+    @property
+    def created_at(self) -> CreatedAt:
+        return self._created_at
+
+    @property
+    def updated_at(self) -> UpdatedAt:
+        return self._updated_at
